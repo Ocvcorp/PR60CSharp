06e5034549d98740f3c6477c5eca7a02fd560a1a
Ocvcorp <ocvcorp123@outlook.com>
2020-07-20 22:27:51 +0300

    Классы Структуры Перечисления
    * каждый пункт - 2 файла: Main и Файл соответствующих типов данных
    1_ Геометрические фигуры в виде структурных типов
    2_ Конвейер-структура с командами в виде перечисления
    3_  Геометрические фигуры в виде классов
    4_ Класс здания
    5_ Классы передвижных средств

---
+++ 4_ClassesStructuresEnums/5_Main_V_Classes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameClasses;
+
+namespace ConsoleApp
+{
+    class Main_lab3_3
+    {
+        static void Main()
+        {
+            //start condition
+            Point startPnt=new Point(0,0);
+            HorseCarriage hc1 = new HorseCarriage("horse", startPnt);
+            Car car1 = new Car("car", startPnt);
+            Airplane air1 = new Airplane("airplane", startPnt);
+            Boat b1 = new Boat("Boat", startPnt);
+            Console.WriteLine("***************Initial condition********************");
+            Console.WriteLine("{0,10}\t{1,11}\t{2,10}", "Vehicle", "Point", "Resource");
+            hc1.showInfo();
+            car1.showInfo();
+            air1.showInfo();
+            b1.showInfo();
+            //after moving
+            Point newPnt1 = new Point(10, 10);
+            Point newPnt2 = new Point(-100, 100);
+            Point newPnt3 = new Point(1500, 330);
+            Point newPnt4 = new Point(50, 10);
+            hc1.move(newPnt1);
+            car1.move(newPnt2);
+            air1.move(newPnt3);
+            b1.move(newPnt4);
+
+            Console.WriteLine("********************After moving**********************");
+            Console.WriteLine("{0,10}\t{1,11}\t{2,10}", "Vehicle", "Point", "Resource");
+            hc1.showInfo();
+            car1.showInfo();
+            air1.showInfo();
+            b1.showInfo();
+        }
+    }
+}
