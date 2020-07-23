b9bd31c5c03c706783db4cf5d2cb7bbda67e1856
Ocvcorp <ocvcorp123@outlook.com>
2020-07-23 23:20:10 +0300

    Библиотека TPL. Класс Parallel. PLINQ
    1_ отмена работы 2х классов по кодовому слову
    2_ обработка массива с использованием TPL
    3_ параллельное преобразование массива тремя методами Parallel
    4_Parallel.For  сложение и умножение матриц
    5_ PLINQ запросы для выбора данных
    6_ Параллельный ввод/вывод из файлов с PLINQ -выборкой

---
+++ 13_TPL_Parallel/5_TPL_PLINQ.cs
@@ -0,0 +1,114 @@
+﻿/*
+ 1.	Реализовать все описанные случаи, используя синтаксические элементы LINQ:
+•	Запрос и вывод на экран товарных запасов — список товаров, запасы которых исчерпались на складе; 
+2.	Источник данных – это массив элементов или коллекция. Размер – не менее 20 строк.
+3.	В каждом случае использовать составной тип данных (класс или структуру) для хранения строк таблиц. 
+Создать не менее 5-ти полей (членов-переменных примитивного типа) для каждого составного типа данных.
+4.	Создать сложные запросы для выбора данных из источника по нескольким полям.
+5.	Использовать методы AsParallel, AsOrdered библиотеки TPL для PLINQ, чтобы обеспечить параллелизм данных внутри запроса.
+6.	Организовать отмену задачи в запросе для случая некорректно введенных условий выбора строк источника данных.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public struct Goods
+    {
+        public int number;
+        public string name;
+        public string category;
+        public double price;
+        public int qnty;
+        public static void PrintStore(IEnumerable<Goods> choosing, string preText="")
+        {
+            Console.WriteLine(preText+"\nNumber\tName\tCategory\tPrice\tQuantity");
+            foreach (Goods g in choosing)
+                Console.WriteLine("{0}\t{1}\t{2}\t{3:F2}\t{4}",
+                    g.number,g.name,g.category, g.price, g.qnty);
+        }
+        
+    }
+
+    class lab15_1_TPL_PLINQ
+    {        
+        static void Main(string[] args)
+        {
+            List<Goods> store = new List<Goods>()
+            {
+                new Goods { number=01, name="Apples", category="Fruits", price=10.2, qnty=200},
+                new Goods { number=02, name="Bananas", category="Fruits", price=15.2, qnty=0},
+                new Goods { number=03, name="PineApples", category="Fruits", price=110.2, qnty=150},
+                new Goods { number=04, name="Peach", category="Fruits", price=10.3, qnty=220},
+                new Goods { number=05, name="Strawberry", category="Fruits", price=15.6, qnty=0},
+                new Goods { number=06, name="Raspberry", category="Fruits", price=18.3, qnty=0},
+                new Goods { number=07, name="Milk", category="MilkProducts", price=20.2, qnty=1200},
+                new Goods { number=08, name="Youguart", category="MilkProducts", price=5.11, qnty=0},
+                new Goods { number=09, name="Butter", category="MilkProducts", price=16.1, qnty=220},
+                new Goods { number=10, name="Cheese", category="MilkProducts", price=21.3, qnty=230},
+                new Goods { number=11, name="Buckweet", category="Porrigies", price=15.21, qnty=2000},
+                new Goods { number=12, name="Rise", category="Porrigies", price=11.1, qnty=150},
+                new Goods { number=13, name="Hercules", category="Porrigies", price=13.9, qnty=0},
+                new Goods { number=14, name="Manka", category="Porrigies", price=9.21, qnty=200},
+                new Goods { number=15, name="Saussage", category="Meat Products", price=22.2, qnty=0},
+                new Goods { number=16, name="Bekon", category="Meat Products", price=25.0, qnty=2000},
+                new Goods { number=17, name="Ham", category="Meat Products", price=33.3, qnty=2001},
+                new Goods { number=18, name="Salo", category="Meat Products", price=51.2, qnty=0},
+                new Goods { number=19, name="Carbonade", category="Meat Products", price=43.2, qnty=2200},
+                new Goods { number=20, name="Sordelki", category="Meat Products", price=22.7, qnty=10}
+            };
+
+            //1.choose 0 remain in store
+            var res1 = from g in store
+                       where g.qnty == 0
+                       select g;
+            Goods.PrintStore(res1, "Choosing items with no remain");
+            //2.choose item with price>15 from MilkProducts
+            var res2 = from g in store
+                       where g.category == "MilkProducts" 
+                       where g.price > 15.0
+                       select g;
+            Goods.PrintStore(res2, "Choosing items from MilkProducts with price 15.0+");
+            //3.choose 0 remain in store + asParallel+ asOrdered
+            var resPar = from g in store.AsParallel().AsOrdered()
+                         where g.qnty == 0
+                         select g;
+            Goods.PrintStore(res1, "Choosing items with no remain in Parallel mode (order saved)");
+            //4.task cancel if incorrect choose condition
+            double maxPrice = -20.2;
+            CancellationTokenSource cancelTokSrc = new CancellationTokenSource();
+            var res4 = from g in store.AsParallel().AsOrdered().WithCancellation(cancelTokSrc.Token)
+                       where g.price < maxPrice
+                       select g;
+            Task cancelTsk = Task.Factory.StartNew(() =>
+            {
+                if (maxPrice<=0) 
+                    cancelTokSrc.Cancel();
+            });
+            try
+            {
+                Goods.PrintStore(res4, "Choosing items with price less then "+maxPrice);
+            }
+            catch (OperationCanceledException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (AggregateException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            finally
+            {
+                cancelTsk.Wait();
+                cancelTokSrc.Dispose();
+                cancelTsk.Dispose();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 13_TPL_Parallel/6_Parallel_PLINQ.cs
@@ -0,0 +1,135 @@
+﻿/*
+1.	Реализовать параллельный ввод-вывод для четырех различных текстовых файлов 
+с использованием средств параллельных запросов PLINQ.
+•	имена файлов 1.txt, 2.txt, 3.txt, 4.txt
+•	каждый файл содержит не менее 20 строк
+•	каждая строка содержит не менее 20 случайных двузначных натуральных чисел 
+2.	Необходимо выполнить параллельный ввод каждой очередной строки из каждого файла в отдельной задаче, 
+генерируемые общим параллельным запросом
+3.	Рассчитать для каждой строки абсолютное значение полусуммы минимального и максимального элементов 
+4.	Сохранить последовательность данных источника с использованием метода AsOrdered
+5.	Осуществить расчет полусуммы с минимальной задержкой потока 100 мс
+6.	Вывести в консоль значения полусуммы каждой строки каждого из 4-х файлов, объединяя их в одну строку, 
+и разделяя запятыми. Например:
+23, 5, 41, 90 
+34, 6, 76, 24
+13, 7, 76, 77
+..................... - общее количество строк - не менее 20 
+ */
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class lab15_2_Parallel_PLINQ
+    {
+        static void FilesGenerate()
+        {
+            StreamWriter sw;
+            Random rnd=new Random();
+            string fileline;
+            for (int i=1; i<5; i++)
+            {
+                sw = new StreamWriter(i + ".txt");
+                for (int j = 0; j < 20; j++)
+                {
+                    fileline = "";
+                    for (int k = 0; k < 20; k++)
+                    {
+                        fileline += (rnd.Next(10, 99).ToString() + " ");
+                    }
+                    fileline += (rnd.Next(10, 99).ToString());
+                    sw.WriteLine(fileline);
+                }
+                sw.Close();
+            }
+        }
+        static double GetSemiSum(string strNumbers, bool isDelayed)
+        {
+            double sum = 0;
+            if (strNumbers!=null)
+            {
+                foreach (var num in strNumbers)
+                    sum += (double)num;
+                if (isDelayed)
+                    Thread.Sleep(100);               
+                return sum / 2;
+            }
+            return 0;
+        }
+        static void Main(string[] tArgs)
+        {
+            //FilesGenerate();//files generating
+            
+            StreamReader[] files = 
+                {
+                    new StreamReader("1.txt"),
+                    new StreamReader("2.txt"),
+                    new StreamReader("3.txt"),
+                    new StreamReader("4.txt")
+                };
+            ConcurrentBag<string> concurrentBag;
+            for (int i=0;i<20;i++)
+            {
+                concurrentBag = new ConcurrentBag<string>();
+                var query = from q in files.AsParallel()
+                            select GetSemiSum(q.ReadLine(), true).ToString();
+                query.ForAll(e => concurrentBag.Add(e));
+                Console.WriteLine(string.Join(", ", concurrentBag.ToArray()));
+            }
+            foreach (var f in files)
+                f.Close();
+
+            Console.WriteLine("In order");
+            StreamReader[] files3 =
+                {
+                    new StreamReader("1.txt"),
+                    new StreamReader("2.txt"),
+                    new StreamReader("3.txt"),
+                    new StreamReader("4.txt")
+                };
+
+            for (int i = 0; i < 20; i++)
+            {
+                concurrentBag = new ConcurrentBag<string>();
+                var query = from q in files3.AsParallel().AsOrdered<StreamReader>()
+                            select GetSemiSum(q.ReadLine(), true).ToString();
+                string[] data = query.ToArray();
+                Console.WriteLine(string.Join(", ", data));
+            }
+            foreach (var f in files3)
+                f.Close();
+
+            Console.WriteLine("Checking without parallel computing");
+            StreamReader[] files2 =
+                {
+                    new StreamReader("1.txt"),
+                    new StreamReader("2.txt"),
+                    new StreamReader("3.txt"),
+                    new StreamReader("4.txt")
+                };
+            int k=0;
+            string[,] vs = new string[20, 4];
+            foreach (StreamReader f in files2)
+            {                
+                for (int i=0;i<20;i++)
+                {             
+                    vs[i, k] = GetSemiSum(f.ReadLine(), false).ToString();
+                }
+                f.Close();
+                k++;
+            }
+            for (int i=0;i<20;i++)
+            {
+                Console.WriteLine("{0:F1}, {1:F1}, {2:F1}, {3:F1}", vs[i, 0], vs[i, 1], vs[i, 2], vs[i, 3]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
