d5ca1d4cdeaac2cc01ef9de9bb5632d8d0af4fcb
Ocvcorp <ocvcorp123@outlook.com>
2020-07-23 23:47:47 +0300

    UWP
    1_ Эксперименты с Xaml

---
+++ 16_UWP/1_ UwpXamlWinUI/UwpXamlSample/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace UwpXamlSample
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = !splitView.IsPaneOpen;
+            
+        }
+        private void SplitView_Open(SplitView sender, object args)
+        {
+            text1.Text = "Панель открыта";
+            text2.Text = "Ого!";
+        }
+        private void SplitView_Close(SplitView sender, object args)
+        {
+                text1.Text = "Панель закрыта";
+                text2.Text = "Ага!";
+        }
+    }
+}
